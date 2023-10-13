6de26aa9ef9d3562fdcbea8d09b193bf9e980c72
Islam <aitemr16@gmail.com>
2023-10-13 19:05:34 +0600

    test

--- gitsample/ViewController.swift
+++ gitsample/ViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.font = .boldSystemFont(ofSize: 28)
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .red
+
+        title = "Title"
+
+        view.addSubview(titleLabel)
     }
 }
 
